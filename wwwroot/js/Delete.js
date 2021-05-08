1cf7252e254daa5f1381247aacf675d84fe8202f
Maari-Thelearner <bs.mari100@gmail.com>
2021-05-08 21:39:07 +0530

    initial commit

---
+++ wwwroot/js/Delete.js
@@ -0,0 +1,14 @@
+//Function to confirm delete
+
+function confirmDelete(uniqueID, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueID;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueID;
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
